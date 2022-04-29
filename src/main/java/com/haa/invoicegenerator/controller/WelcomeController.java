3c256269cec0fc2c89a375da7e070c5c97989515
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2022-04-29 07:41:14 +0530

    invoice dashboard added

--- src/main/java/com/haa/invoicegenerator/controller/WelcomeController.java
+++ src/main/java/com/haa/invoicegenerator/controller/WelcomeController.java
@@ -1,5 +1,11 @@
 package com.haa.invoicegenerator.controller;
 
+import java.util.List;
+
+import com.haa.invoicegenerator.entity.InvoiceDetails;
+import com.haa.invoicegenerator.service.InvoiceDetailsService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +13,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WelcomeController {
 
+    @Autowired
+    private InvoiceDetailsService invoiceService;
+
     @GetMapping(value = "/")
     public String getDefectList(Model theModel) {
+        List<InvoiceDetails> invoiceList = invoiceService.getAllInvoice();
+
+        theModel.addAttribute("invoiceList", invoiceList);
+
         return "home";
     }
 
